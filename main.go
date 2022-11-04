24e1afd6563ac1ed0231274851ba0e146e6127e8
Richd0tcom <tuberich0@gmail.com>
2022-11-04 10:04:28 +0100

    feat: add atrribute field

--- main.go
+++ main.go
@@ -11,29 +11,31 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 type NFT struct {
-	Format string `json:"format"`
-	Name         string `json:"name"`
-	Description  string `json:"description"`
-	MintingTool string `json:"minting_tool"`
-	SensitiveContent bool `json:"sensitive_content"`
-	SeriesNumber int    `json:"series_number"`
-	SeriesTotal int `json:"series_total"`
-	Collection Collections `json:"collection"`
-	Gender       string `json:"gender"`
-	Uuid         string `json:"uuid"`
-	Hash         string `json:"hash"`
+	Format           string      `json:"format"`
+	Name             string      `json:"name"`
+	Description      string      `json:"description"`
+	MintingTool      string      `json:"minting_tool"`
+	SensitiveContent bool        `json:"sensitive_content"`
+	SeriesNumber     int         `json:"series_number"`
+	SeriesTotal      int         `json:"series_total"`
+	Attributess      []Trait     `json:"attributes"`
+	Collection       Collections `json:"collection"`
+	Gender           string      `json:"gender"`
+	Uuid             string      `json:"uuid"`
 }
 
 type Collections struct {
-	Name string	 `json:"name"`
-	Id   string `json:"id"`
+	Name       string       `json:"name"`
+	Id         string       `json:"id"`
 	Attributes []Attributes `json:"attributes"`
 }
+
 type Attributes struct {
-	Type string `json:"type"`
+	Type  string `json:"type"`
 	Value string `json:"value"`
 }
 
@@ -48,14 +50,11 @@ func main() {
 	888   Y8888 888            	888           888        888  888 888          X88 Y8b.     888     
 	888    Y888 888            	888           888        "Y888888 888      88888P'  "Y8888  888     `)
 
-
-
 	var inputFileName string
-	flag.StringVar(&inputFileName, "input", "" ,"A csv input file for the nfts")
+	flag.StringVar(&inputFileName, "input", "", "A csv input file for the nfts")
 	flag.Parse()
-	
+
 	outputCsv := filepath.Base(inputFileName)
-	
 
 	csvFile, err := os.Open(inputFileName)
 	if err != nil {
@@ -64,15 +63,13 @@ func main() {
 	}
 	reader := csv.NewReader(csvFile)
 
-
 	//create a new output file.
 	var filename = outputCsv
 	var extension = filepath.Ext(filename)
 	fmt.Println(extension)
-	var newCsvBase = filename[0:len(filename)-len(extension)]
+	var newCsvBase = filename[0 : len(filename)-len(extension)]
 
-
-	file, err := os.Create(newCsvBase+".output.csv")
+	file, err := os.Create(newCsvBase + ".output.csv")
 	if err != nil {
 		fmt.Println("Error creating csv file: ", err)
 		panic(err)
@@ -80,12 +77,12 @@ func main() {
 	writer := csv.NewWriter(file)
 	if _, err := os.Stat("nft"); os.IsNotExist(err) {
 
-        os.Mkdir("nft", 0777)
-        fmt.Println("Directory created, Parsing nfts...")
-    } else {
+		os.Mkdir("nft", 0777)
+		fmt.Println("Directory created, Parsing nfts...")
+	} else {
 
-        fmt.Println("Directory already exists,Parsing nfts...")
-    }
+		fmt.Println("Directory already exists,Parsing nfts...")
+	}
 
 	var nfts NFT
 
@@ -93,46 +90,72 @@ func main() {
 	line = append(line, "sha256")
 	writer.Write(line)
 
+	counter := 1
+
 	for {
+
 		line, errors := reader.Read()
 		if errors == io.EOF {
 			break
 		} else if errors != nil {
 			log.Fatal(errors)
 		}
-		num, _ := strconv.Atoi(line[0])
+		teamName := getTeamName(counter)
+
+		num, _ := strconv.Atoi(line[1])
+		// var attri []string
+
+		atr := strings.Split(line[6], ";")
+		items := []Trait{}
+		for _, ele := range atr {
+
+			attri := strings.Split(ele, ":")
+			// if len(attri)>0{
+			// 	attri = attri[:len(attri)-1]
+			// }
+
+			if len(attri) == 2 {
+				at := Trait{
+					TraitType: attri[0],
+					Value:     attri[1],
+				}
+				fmt.Println(at)
+				items = append(items, at)
+			}
+
+		}
 		nfts = NFT{
-			Format : "CHIP-0007",
-			
-			Name:         line[1],
-			Description:  line[2],
-			MintingTool: newCsvBase,
+			Format: "CHIP-0007",
+
+			Name:             line[3],
+			Description:      line[4],
+			MintingTool:      teamName,
 			SensitiveContent: false,
-			SeriesNumber: num,
-			SeriesTotal: 420,
-			// Gender:       line[3],
-			Uuid:         line[4],
-			Hash:         line[5],
+			SeriesNumber:     num,
+			SeriesTotal:      420,
+			Gender:           line[5],
+			Attributess:      items,
+			Uuid:             line[7],
 			Collection: Collections{
 				Name: "Zuri NFT Tickets for Free Lunch",
-				Id: "b774f676-c1d5-422e-beed-00ef5510c64d",
+				Id:   "b774f676-c1d5-422e-beed-00ef5510c64d",
 				Attributes: []Attributes{
 					{
-						Type: "description",
+						Type:  "description",
 						Value: "Rewards for accomplishments during HNGi9.",
 					},
 				},
 			},
 		}
 
-		nftJson, _ := json.Marshal(nfts)
+		nftJson, _ := json.MarshalIndent(nfts, "", "  ")
 		// nftJsonFile:= fmt.Sprintf()
-		err = os.WriteFile("nft/"+line[1]+".json", nftJson, 0777)
+		err = os.WriteFile("nft/"+line[2]+".json", nftJson, 0777)
 		if err != nil {
 			panic(err)
 		}
-		f, err := os.Open("nft/"+line[1]+".json")
-		
+		f, err := os.Open("nft/" + line[2] + ".json")
+
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -142,19 +165,92 @@ func main() {
 		if _, err := io.Copy(h, f); err != nil {
 			log.Fatal(err)
 		}
-		x:=h.Sum(nil)
+		x := h.Sum(nil)
 		line = append(line, fmt.Sprintf("%x", x))
 		_ = writer.Write(line)
 
 		sha256.New().Reset()
+
+		counter++
 	}
 	writer.Flush()
 	//...................................
 	//Writing struct type to a JSON file
 	//...................................
-	
 
 	//Next use the flag package to prompt the user for input
 
+}
 
+func getTeamName(counter int) string {
+	var teamName string
+	switch {
+	case counter > 400:
+		teamName = "Team Boot"
+
+	case counter > 380:
+		teamName = "Team Axe"
+	case counter > 360:
+		teamName = "Team VBelt"
+
+	case counter > 340:
+		teamName = "Team Ruler"
+
+	case counter > 320:
+		teamName = "Team Sandpaper"
+
+	case counter > 300:
+		teamName = "Team Scale"
+
+	case counter > 280:
+		teamName = "Team Hydraulics"
+
+	case counter > 260:
+		teamName = "Team Powerdrill"
+
+	case counter > 240:
+		teamName = "Team Axle"
+
+	case counter > 220:
+		teamName = "Team Tape"
+
+	case counter > 200:
+		teamName = "Team Gear"
+
+	case counter > 180:
+		teamName = "Team Crankshaft"
+
+	case counter > 160:
+		teamName = "Team Headlight"
+
+	case counter > 140:
+		teamName = "Team Plug"
+
+	case counter > 120:
+		teamName = "Team Brainbox"
+
+	case counter > 100:
+		teamName = "Team Chisel"
+
+	case counter > 80:
+		teamName = "Team Prybar"
+
+	case counter > 60:
+		teamName = "Team Grit"
+
+	case counter > 40:
+		teamName = "Team Engine"
+
+	case counter > 20:
+		teamName = "Team Clutch"
+
+	default:
+		teamName = "Team Bevel"
+	}
+	return teamName
+}
+
+type Trait struct {
+	TraitType string `json:"trait_type"`
+	Value     string `json:"value"`
 }
